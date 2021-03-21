fb47e0c7276ae4e86ee51f37c35568c30cba6f61
Fabian Meyer <fm@noinfo.de>
2021-03-21 16:41:37 +0100

    Refactor to use path tool; add help; rename file

--- watch_and_version.js
+++ watch_and_version.js
@@ -1,13 +1,17 @@
+#!/usr/bin/env node
 const chokidar = require('chokidar');
 const fs = require('fs');
 const path = require('path');
 const zeropad = (number) => ("0000" + number).slice(-4);
+
 const showHelp = () => {
-  // TODO write comprehensive help
-  console.log(`\nThis is the help`);
+  console.log(`\n${path.basename(process.argv[1])} filename_to_watch.for\n\n
+Will move "filename_to_watch.for" to "filename_to_watch[counter].for" whenever it is created counting up.
+Example: filename_to_watch0001.for
+\n`);
 };
 
-if(process.argv.length < 3){
+if(process.argv.length < 3 || (process.argv.length >= 3 && (process.argv[2] === "-h" || process.argv[2] === "--help"))){
   showHelp();
   return 1;
 }
@@ -19,23 +23,23 @@ if(watchedFilePath.indexOf("/") >= 0){
 }
 
 // get file parts
-const watchedFileArray = watchedFilePath.split(".");
-const fileExtension = watchedFileArray[watchedFileArray.length - 1];
-const filePrefix = watchedFilePath.replace("." + fileExtension, "");
+const fileExtension = path.extname(watchedFilePath);
+const filePrefix = watchedFilePath.replace(fileExtension, "");
+const fileBasenamePrefix = path.basename(watchedFilePath).replace(fileExtension, "");
 console.log(`Running on "${watchedFilePath}"\nPrefix: ${filePrefix}\nExtension: ${fileExtension}\n`);
 // now go through the folder and determine the highest number
 const folder = fs.readdirSync(folderPath);
 let counter = 0;
 folder.forEach(file => {
-  if(file.startsWith(filePrefix)){
-    const numberString = file.replace("." + fileExtension, "").replace(filePrefix, "");
+  if(file.startsWith(fileBasenamePrefix)){
+    const numberString = file.replace(fileExtension, "").replace(fileBasenamePrefix, "");
     const number = parseInt(numberString, 10);
     if(!isNaN(number) && counter < number){
       counter = number;
     }
   }
 })
-console.log(`found files and calculated counter to be: ${counter} / zeropadded: ${zeropad(counter)}`);
+console.log(`Counter starts at: ${counter} / zeropadded: ${zeropad(counter)}`);
 
 // watch for events using chokidar
 chokidar.watch(`${folderPath}*`).on('all', (event, path) => {
@@ -43,8 +47,9 @@ chokidar.watch(`${folderPath}*`).on('all', (event, path) => {
   if(path.indexOf(watchedFilePath) >= 0){
     switch (event) {
       case "add":
-        console.log(`Added watched file: ${path}`);
-        fs.renameSync(path,`${filePrefix}${zeropad(++counter)}.${fileExtension}`);
+        const newfilename = `${filePrefix}${zeropad(++counter)}${fileExtension}`;
+        console.log(`Added watched file: ${path}\n\tmoved to: ${newfilename}`);
+        fs.renameSync(path,newfilename);
         break;
       case "change":
         // TODO add option to "react & copy" instead of "move on create"
